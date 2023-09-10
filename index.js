c20d65172ff5fb61a86947c08146b7fe8c2c81a6
Andrey <soldatenkov067@bk.ru>
2023-09-10 21:37:02 +0300

    Add hw_2

---
+++ index.js
@@ -0,0 +1,12 @@
+// Задание 1
+
+let degreesCelsius = 20;
+let degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
+
+console.log(`Температура в градусах Цельсия ${degreesCelsius}, а по Фаренгейту ${degreesFahrenheit}`);
+
+// Задание 2
+
+let name = 'Andrey';
+let admin = name;
+console.log(admin);
